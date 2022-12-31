d322c31c4d1faa519c05805a461913325acb1b39
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2022-12-31 13:16:34 +0600

    last Commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = 5000;
+
+const port = process.env.PORT;
 
 // moddlewares
 app.use(cors());
 app.use(express.json());
+
 
 
 // MongoDB connection
@@ -18,8 +21,8 @@ app.use(require('./routes/route'));
 conn.then(db=>{
     if(!db) return process.exit(1)
 
-    app.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
     })
 
     app.on('error', err => console.log(`Failed to connect with HTTP server: ${err}`))
